ae819fe2bf4f938838611d6057bac90841a1024b
Ankita Garg <gargankita9789@gmail.com>
2020-11-01 01:39:05 +0530

    Operations Added

--- MyCalculator/controller.java
+++ MyCalculator/controller.java
@@ -1,9 +1,40 @@
 package MyCalculator;
 
-import java.awt.TextField;
 import java.awt.event.ActionEvent;
+import java.util.*;
 
 public class controller extends Calculator{
+	static float result = 0, num1 = 0;
+	static Queue<Character> q = new LinkedList<>();
+	static void calculateAns(char op)
+	{
+		switch (op)
+		{
+		case '/':
+			try
+			{
+				result /= num1;
+				str.replace(0, str.length(), Float.toString(result));
+			}
+			catch (Exception e)
+			{
+				str.replace(0, str.length(), "Exception divide by zero");
+			}
+			break;
+		case '*':
+			result *= num1;
+			str.replace(0, str.length(), Float.toString(result));
+			break;
+		case '+':
+			result += num1;
+			str.replace(0, str.length(), Float.toString(result));
+			break;
+		case '-':
+			result -= num1;
+			str.replace(0, str.length(), Float.toString(result));
+			break;
+		}
+	}
 	static void calc(ActionEvent e)
 	{
 		if (e.getSource() == btnAc)
@@ -11,9 +42,15 @@ public class controller extends Calculator{
 		if (e.getSource() == btnBs && str.length() > 0)
 			str.delete(str.length() - 1, str.length());
 		if (e.getSource() == btnNewButton)
-			str.append("/");
+		{
+			str.append('/');
+			q.add('/');
+		}
 		if (e.getSource() == button_3)
-			str.append("*");
+		{
+			str.append('*');
+			q.add('*');
+		}
 		if (e.getSource() == button_4)
 			str.append("1");
 		if (e.getSource() == button_5)
@@ -21,7 +58,10 @@ public class controller extends Calculator{
 		if (e.getSource() == button_6)
 			str.append("3");
 		if (e.getSource() == button_7)
-			str.append("-");
+		{
+			str.append('-');
+			q.add('-');
+		}
 		if (e.getSource() == button_8)
 			str.append("4");
 		if (e.getSource() == button_9)
@@ -29,7 +69,10 @@ public class controller extends Calculator{
 		if (e.getSource() == button_10)
 			str.append("6");
 		if (e.getSource() == button_11)
-			str.append("+");
+		{
+			str.append('+');
+			q.add('+');
+		}
 		if (e.getSource() == button_12)
 			str.append("7");
 		if (e.getSource() == button_13)
@@ -40,6 +83,47 @@ public class controller extends Calculator{
 			str.append(".");
 		if (e.getSource() == button_17)
 			str.append("0");
+		if (e.getSource() == button)
+		{
+			float k = Float.parseFloat(str.toString());
+			k = -k;
+			str.replace(0, str.length(), Float.toString(k));
+		}
+		if (e.getSource() == button_15)
+		{
+			if (str.charAt(str.length() - 1) < '0' || str.charAt(str.length() - 1) > '9')
+				str.replace(0, str.length(), "Invalid Syntax"); 
+			else
+			{
+				String num = "";
+				for (int i = 0; i < str.length(); i++)
+				{
+					if ((str.charAt(i) >= '0' && str.charAt(i) <= '9') || str.charAt(i) == '.')
+						num += str.charAt(i);
+					else
+					{
+						num1 = Float.parseFloat(num);
+						System.out.println(num1);
+						num = "";
+						if (result == 0)
+							result = num1;
+						else
+							calculateAns(q.poll());						
+					}
+				}
+				num1 = Float.parseFloat(num);
+				System.out.println(num1);
+				if (result == 0)
+					result = num1;
+				else
+					if (!q.isEmpty())
+					calculateAns(q.poll());
+				
+				num1 = 0;
+				q.clear();
+				result = 0;
+			}
+		}
 		textField.setText(str.toString());
 			
 	}
