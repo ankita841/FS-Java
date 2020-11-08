491d514cd8149871b8b88cb4f6022584918d641e
Ankita Garg <gargankita9789@gmail.com>
2020-11-09 02:25:54 +0530

    Wipro

--- InventoryAndSalesSystem/src/com/wipro/sales/dao/StockDao.java
+++ InventoryAndSalesSystem/src/com/wipro/sales/dao/StockDao.java
@@ -1,17 +1,81 @@
 package com.wipro.sales.dao;
 
+import com.wipro.sales.bean.*;
+import com.wipro.sales.util.DBUtil;
+
+import java.sql.*;
 import java.util.*;
 
 public class StockDao {
+	PreparedStatement ps;
+	Connection con;
 	
-	void insertStock(Stock sales)
+	void insertStock(Product sales)
+	{
+		con = DBUtil.getDBConnection();
+		try
+		  {
+			  ps = con.prepareStatement("INSERT into TBL_SALES values (?, ?, ?, ?, ?)");
+			  ps.setString(1, sales.getProductID());
+			  ps.setString(2,  sales.getProductName());
+			  ps.setInt(3,  sales.getQuantityOnHand());
+			  ps.setDouble(4,  sales.getProductUnitPrice());
+			  ps.setInt(5, sales.getReorderLevel());
+			  ps.executeUpdate();
+		  }
+		  catch (Exception e)
+		  {
+			  e.printStackTrace();
+		  }
+	}
+	
+	String generateProductID(String productName)
+	{
+		try
+		{
+			 ps = con.prepareStatement("SELECT last_number from user_sequences where sequence_name = '<SEQ_PRODUCT_ID>'");
+			  ResultSet rs = ps.executeQuery();
+			  if (rs.next())
+				  return productName.substring(0, 2) + rs.getInt(1);			
+		}
+		catch (Exception e)
+		{
+			e.printStackTrace();
+		}
+		return "";
+	}
+	
+	void updateStock(String productID, int soldQty)
 	{
 		
 	}
 	
-	void generateProductID(String productName)
+	Product getStock(String productID) throws Exception
 	{
-		
+		Product stock = new Product();
+		con = DBUtil.getDBConnection();
+		Statement st = con.createStatement();
+		ResultSet rs = st.executeQuery("SELECT * FROM TBL_STOCK WHERE productID='" + productID + "'");
+	    stock.setProductID(rs.getString("productID"));
+	    stock.setProductName(rs.getNString("productName"));
+	    stock.setQuantityOnHand(rs.getInt("quantityOnHand"));
+	    stock.setProductUnitPrice(rs.getDouble("productUnitPrice"));
+	    stock.setReorderLevel(rs.getInt("reorderLevel"));
+		return stock;
+	}
+	
+	void deleteStock(String productID)
+	{
+		try
+		{
+			ps = con.prepareStatement("DELETE from TBL_STOCK where product_id=?");
+			ps.setString(1, productID);
+			ps.executeQuery();
+		}
+		catch (Exception e)
+		{
+			e.printStackTrace();
+		}
 	}
 
 }
